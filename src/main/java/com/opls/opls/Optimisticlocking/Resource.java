63a0ecce75e7da76c822a7c564f8113e6f01e1dc
ABAlosaimi <afalih.alosaimi@gmail.com>
2026-01-22 14:57:07 +0300

    the main entity major update

--- src/main/java/com/opls/opls/Optimisticlocking/Resource.java
+++ src/main/java/com/opls/opls/Optimisticlocking/Resource.java
@@ -2,33 +2,28 @@ package com.opls.opls.Optimisticlocking;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "resource")
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Resource {
 
     @Id
-    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "resource_meta_data", nullable = false, unique = true, updatable = false)
-    @NotNull
-    @NotBlank
-    private String resourceMetaData;
+   // finding a way to store the meta data of the resource 
 
     @Column(name = "version", nullable = false, updatable = true)
     @NotNull
-    private int version;
+    private Long version;
 
-    
 }
