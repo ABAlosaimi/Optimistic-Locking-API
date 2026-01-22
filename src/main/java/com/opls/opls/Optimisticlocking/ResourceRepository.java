a9988d873f105f9c28565ef544f40d8146edef00
ABAlosaimi <afalih.alosaimi@gmail.com>
2026-01-22 14:46:22 +0300

    fixed a previous syntax bug in the repo

--- src/main/java/com/opls/opls/Optimisticlocking/ResourceRepository.java
+++ src/main/java/com/opls/opls/Optimisticlocking/ResourceRepository.java
@@ -2,10 +2,9 @@ package com.opls.opls.Optimisticlocking;
 
 import java.util.Optional;
 
-import org.springframework.data.jpa.repository.JpaRepository;ت
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ResourceRepository extends JpaRepository<Resource, Long> {
 
-    Optional<Resource> findByIdAndVersionAndResourceMetaData(Long Id, int version, String meta) throws Exception;
-    
+    Optional<Resource> findByIdAndVersionAndResourceMetaData(Long Id, int version, String meta) throws Exception;  
 }
